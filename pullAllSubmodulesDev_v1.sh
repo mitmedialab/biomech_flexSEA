2f3f81bf932c82338586b6fdb5e83f21f8e61bdb
Jean-Francois Duval <jfduval@aqra.ca>
2017-08-30 10:01:15 -0400

    Basic script to update all submodules at once

---
+++ pullAllSubmodulesDev_v1.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# To run this script on Windows, simply call "./pullAllSubmodulesDev_v1.sh"
+
+echo 
+echo [This script will pull the latest version of the 4 typical submodules]
+echo 
+
+echo flexsea-user...
+cd flexsea-user
+git pull
+cd ..
+echo 
+
+echo flexsea-comm...
+cd flexsea-comm
+git pull
+cd ..
+echo 
+
+echo flexsea-system...
+cd flexsea-system
+git pull
+cd ..
+echo 
+
+echo flexsea-shared...
+cd flexsea-shared
+git pull
+cd ..
+echo 
+
+echo [Done]
+echo 
